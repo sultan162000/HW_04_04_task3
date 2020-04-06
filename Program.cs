7aed36958cc01d0f12f1876a78d0e1fabfa212c1
Кодиров Султон <sultonkodrio109@gmail.com>
2020-04-06 11:26:37 +0500

    Project change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,62 @@ namespace HW_04_04_task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee Sulton = new Employee("Sulton","Qodirov",1);
         }
+    }
+    class Employee{
+        public string name{get;set;}
+        public string lastName{get;set;}
+        private int Experience;
+        public int experience{
+            get{
+                return Experience;
+            }
+            set{
+                if(value>=0&&value<=50)Experience=value;
+            }
+        }
+        public int salary = 0;
+        public string position;
+
+        public Employee(string name,string lastName,int experience){
+            this.name = name;
+            this.lastName = lastName;
+            this.experience = experience;
+            checkTheExpersiens(experience);
+        }
+
+        public void checkTheExpersiens(int i){
+            if(i<=2&&i>=0){
+                position = "Junior";
+                salary = 1500;
+            }else if(i>=3&&i<=4){
+                position = "Middle";
+                salary = 2500;
+            }else if(i>=5&&i<=7){
+                position = "Senior";
+                salary = 4000;
+            }else if(i<=7&&i>=9){
+                position = "Team Leader";
+                salary = 5000;
+            }else if(i>10){
+                position = "Project Manager";
+                salary = 6000;
+            }
+
+        }
+
+
+        public void inputInfo(){
+            System.Console.WriteLine("Имя: "+name);
+            System.Console.WriteLine("Фамилия: "+lastName);
+            System.Console.WriteLine("Должность: "+position);
+            System.Console.WriteLine("Зарплата: "+salary);
+            
+            
+        }
+
+
+        
     }
 }
