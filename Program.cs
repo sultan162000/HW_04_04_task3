7b1f44dd1f7075c2917c3c813e1ec50f439fdbdd
Кодиров Султон <sultonkodrio109@gmail.com>
2020-04-06 10:56:34 +0500

    New project for HW_04_task3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_04_04_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
